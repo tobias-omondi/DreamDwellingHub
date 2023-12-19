bce2df9ccfe6ef7be60334fe2a81ffee8566caeb
tobias <tobias.omondi@student.moringaschool.com>
2023-12-19 16:56:51 +0300

    app updated

---
+++ server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import db, User,Notifications,News,RatingsReviews,Company,Properties,PropertiesImages,Admin,Agent,Transaction
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///realestate.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate (app,db)
+
+db.init_app(app)
+
+if __name__ =='__main__':
+    app.run(port=5555)
