6d355549e4f7e28054897b2357d6140e15f8e24b
tobias <tobias.omondi@student.moringaschool.com>
2024-01-08 14:46:27 +0300

    seeding

---
+++ server/seed.py
@@ -0,0 +1,5 @@
+from app import app,db
+from models import User,Notifications,RatingsReviews,Transaction,Company,Properties,PropertiesImages,News,Agent,Admin
+from datetime import datetime
+
+# seeding users
